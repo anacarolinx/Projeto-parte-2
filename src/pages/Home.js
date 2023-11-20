c14705cc56482eb8b72c093da99228dccd191641
anacarolinx <ana.00000847943@unicap.br>
2023-11-20 20:23:56 -0300

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import { Div,} from "./style.home";
+import Header from '../Header/Navbar'
+
+function App() {
+  return (
+    <>
+    <Div>
+    <section>
+     <Header/>
+     <main>
+      <h1>A cooperativa Caiçara é uma sociedade constituída por trabalhadores, tendo como objetivo unir todos os pescadores ribeirinhos e do litoral do nordeste, para que trabalhem e atuem na criação de redes de desenvolvimento sustentáveis. Sua unidade fica localizada do bairro do Recife, com funcionamento em horário comercial, sendo de 08:00 às 17:00, de Segunda-feira à Sexta-feira. </h1>
+      
+     
+
+     </main>
+
+    
+     </section>
+     </Div>
+    </>
+    
+  );
+}
+
+export default App;
