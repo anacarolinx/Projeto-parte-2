6bc57b702d4cafd8f41c7a0a09baddb58a0b354a
anacarolinx <111585794+anacarolinx@users.noreply.github.com>
2023-11-20 21:08:30 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,32 +2,22 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Cadastro from "./pages/Cadastro";
-
-// Custom Error Component
-const ErrorComponent = () => {
-  return <div>Error: Página não encontrada!</div>;
-};
+import Cadastro from "./pages/Cadastro"
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          errorElement = <ErrorComponent/>
-          element={
-            <Routes>
-              <Route index element={<Home />} />
-              <Route path="login" element={<Login />} />
-              <Route path="cadastro" element={<Cadastro />} />
-              <Route path="home" element={<Home />} /> />
-            </Routes>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+    <Routes>
+        <Route path="/" element={''}>
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login/>} />
+        <Route path="cadastro" element={<Cadastro/>} />
+        <Route path="home" element={<Home/>} />
+
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
